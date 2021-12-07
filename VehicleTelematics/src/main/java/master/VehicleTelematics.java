b9b854e0b19be0675a8d6f2f69bb9a3a565f355b
JiangWenqi <jiangwenqi1995@gmail.com>
2021-12-07 21:41:45 +0100

    read csv

--- VehicleTelematics/src/main/java/master/VehicleTelematics.java
+++ VehicleTelematics/src/main/java/master/VehicleTelematics.java
@@ -18,38 +18,66 @@
 
 package master;
 
+import master.events.VehicleReport;
+import master.utils.ConfigUtil;
+import org.apache.flink.api.common.eventtime.WatermarkGenerator;
+import org.apache.flink.api.common.eventtime.WatermarkGeneratorSupplier;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.java.io.CsvInputFormat;
+import org.apache.flink.api.java.io.PojoCsvInputFormat;
+import org.apache.flink.api.java.io.RowCsvInputFormat;
+import org.apache.flink.api.java.typeutils.PojoTypeInfo;
+import org.apache.flink.api.java.typeutils.TypeExtractor;
+import org.apache.flink.configuration.MemorySize;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
+import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.time.Duration;
+import java.util.Properties;
+
 
 /**
  * @author Wenqi Jiang & Zhou
  */
 public class VehicleTelematics {
 
-	public static void main(String[] args) throws Exception {
-		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    private static final Logger LOGGER = LogManager.getLogger(VehicleTelematics.class);
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * https://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
+    private static final Properties DEFAULT_PARAMETERS = new ConfigUtil("parameters.properties").getProperties();
 
-		// execute program
-		env.execute("Flink Streaming Java API Skeleton");
-	}
+    public static void main(String[] args) {
+
+        File inputFile = new File(DEFAULT_PARAMETERS.getProperty("input.file"));
+        File outputFile = new File(DEFAULT_PARAMETERS.getProperty("output.file"));
+
+        // set up the streaming execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // read raw data
+        Path inputFilePath = Path.fromLocalFile(inputFile);
+        PojoTypeInfo<VehicleReport> pojoType = (PojoTypeInfo<VehicleReport>) TypeExtractor.createTypeInfo(VehicleReport.class);
+        String[] fieldOrder = new String[]{"timestamp", "vehicleId", "speed", "xWay", "lane", "direction", "segment", "position"};
+        CsvInputFormat<VehicleReport> csvInput = new PojoCsvInputFormat<>(inputFilePath, pojoType, fieldOrder);
+        DataStreamSource<VehicleReport> vehicleReports = env.createInput(csvInput, pojoType);
+        try {
+            vehicleReports.assignTimestampsAndWatermarks(WatermarkStrategy.forBoundedOutOfOrderness(Duration.ofMinutes(1)));
+            env.execute("Vehicle Telematics");
+        } catch (Exception e) {
+            LOGGER.error("start error: {}", e.getMessage());
+            e.printStackTrace();
+        }
+
+
+    }
 }
