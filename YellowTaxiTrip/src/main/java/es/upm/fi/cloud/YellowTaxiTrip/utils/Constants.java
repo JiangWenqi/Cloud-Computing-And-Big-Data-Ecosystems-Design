5ab0602c65b345a918f22b85ea68069b61dc765a
Jiang Wenqi <jiangwenqi1995@gmail.com>
2022-06-27 20:27:37 +0200

    changed the watermark

--- YellowTaxiTrip/src/main/java/es/upm/fi/cloud/YellowTaxiTrip/utils/Constants.java
+++ YellowTaxiTrip/src/main/java/es/upm/fi/cloud/YellowTaxiTrip/utils/Constants.java
@@ -3,6 +3,8 @@ package es.upm.fi.cloud.YellowTaxiTrip.utils;
 import es.upm.fi.cloud.YellowTaxiTrip.models.TaxiReport;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 
+import java.time.Duration;
+
 /**
  * @author wenqi
  */
@@ -10,8 +12,10 @@ public class Constants {
     /**
      * The watermark strategy for the taxi report stream.
      */
-    public static final WatermarkStrategy<TaxiReport> TAXI_REPORT_STRATEGY = WatermarkStrategy.<TaxiReport>forMonotonousTimestamps()
-            .withTimestampAssigner((event, timestamp) -> event.getTpepPickupDatetime().getTime());
+    public static final WatermarkStrategy<TaxiReport> TAXI_REPORT_STRATEGY =
+//            WatermarkStrategy.<TaxiReport>forMonotonousTimestamps()
+            WatermarkStrategy.<TaxiReport>forBoundedOutOfOrderness(Duration.ofMinutes(11))
+                    .withTimestampAssigner((event, timestamp) -> event.getTpepPickupDatetime().getTime());
 
     private Constants() {
     }
